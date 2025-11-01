11341c36cb45f93ad835193da66832c5c8f6d0ee
Fernando Gigliotti <pucca.bruzil@hotmail.com.br>
2025-11-01 15:42:28 -0300

    Adicionando Interceptor para token

--- frontend/src/app/despesas/listar/listar.component.ts
+++ frontend/src/app/despesas/listar/listar.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { CommonModule, DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-listar',
-  imports: [],
+  imports: [CommonModule, DecimalPipe],
   templateUrl: './listar.component.html',
-  styleUrl: './listar.component.css'
+  styleUrls: ['./listar.component.css']
 })
-export class ListarComponent {
+export class ListarComponent implements OnInit {
+  despesas: any[] = [];
+  erro: string = '';
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.carregarDespesas();
+  }
+
+  carregarDespesas() {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      this.erro = 'Usuário não autenticado';
+      return;
+    }
+
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+
+    this.http.get<any[]>('http://localhost:8080/despesas', { headers }).subscribe({
+      next: (data) => {
+        this.despesas = data;
+        this.erro = '';
+      },
+      error: (err) => {
+        console.error(err);
+        this.erro = 'Erro ao carregar despesas';
+      }
+    });
+  }
 }
